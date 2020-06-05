11a06e828fbc7d6abdc260ecf0b9cde0198047b9
CESAR DE MOURA ALMEIDA <61800707+Cesar-Almeida@users.noreply.github.com>
2020-06-05 16:42:44 -0300

    Att

---
+++ Site Naturalização/scripts/planos.js
@@ -0,0 +1,24 @@
+function bordergreen(){
+    document.getElementById("individual").style.backgroundColor="rgba(11, 71, 11, 0.7)";
+    document.getElementById("casal").style.backgroundColor="#ffffffa9";
+    document.getElementById("familia").style.backgroundColor="#ffffffa9";
+    valorindividual.innerHTML='Um pacote simples e dinâmico feito para conseguir a sua dupla nacionalidade';
+    valorcasal.innerHTML='';
+    valorfamilia.innerHTML='';
+}
+function borderblue(){
+    document.getElementById("individual").style.backgroundColor="#ffffffa9";
+    document.getElementById("casal").style.backgroundColor="rgba(11, 20, 71, 0.63)";
+    document.getElementById("familia").style.backgroundColor="#ffffffa9";
+    valorcasal.innerHTML='Um pacote simples e dinâmico feito para conseguir a sua dupla nacionalidade e de seu(sua) parceiro(a)';
+    valorindividual.innerHTML='';
+    valorfamilia.innerHTML='';
+}
+function bordergold(){
+    document.getElementById("individual").style.backgroundColor="#ffffffa9";
+    document.getElementById("casal").style.backgroundColor="#ffffffa9";
+    document.getElementById("familia").style.backgroundColor="rgb(255, 196, 0, 0.6)";
+    valorfamilia.innerHTML='Um pacote simples e dinâmico feito para conseguir a dupla nacionalidade da família toda';
+    valorindividual.innerHTML='';
+    valorcasal.innerHTML='';
+}
